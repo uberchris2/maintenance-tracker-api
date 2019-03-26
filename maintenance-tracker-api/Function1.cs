89f6a1998dbb6b2f769f60e629880a3b2c126ecc
Chris Carpenter <chris@uberchris.com>
2019-03-26 14:17:53 -0500

    testing CD

--- maintenance-tracker-api/Function1.cs
+++ maintenance-tracker-api/Function1.cs
@@ -27,7 +27,7 @@ namespace maintenance_tracker_api
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+                : new BadRequestObjectResult("Who are you?");
         }
     }
 }
