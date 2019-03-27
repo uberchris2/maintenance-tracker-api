995af08948459a2f8e7c030d50ad435b7a3ce6d7
Chris Carpenter <chris@uberchris.com>
2019-03-27 10:57:34 -0500

    add vehicles get endpoint

--- maintenance-tracker-api/Functions/Vehicles.cs
+++ maintenance-tracker-api/Functions/Vehicles.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using maintenance_tracker_api.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -8,8 +10,8 @@ namespace maintenance_tracker_api.Functions
 {
     public static class Vehicles
     {
-        [FunctionName("Vehicles")]
-        public static void Run(
+        [FunctionName("VehiclesPost")]
+        public static void VehiclesPost(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "vehicles")] Vehicle req,
             [CosmosDB(
                 databaseName: "MaintenanceDB",
@@ -19,19 +21,23 @@ namespace maintenance_tracker_api.Functions
             ILogger log
         )
         {
-
             vehicle = req;
             vehicle.id = Guid.NewGuid();
-
             log.LogInformation("Saving new vehicle");
+        }
 
-            //            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //            name = name ?? data?.name;
-            //
-            //            return name != null
-            //                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-            //                : new BadRequestObjectResult("Who are you?");
+        [FunctionName("VehiclesGet")]
+        public static IEnumerable<Vehicle> VehiclesGet(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "vehicles")] HttpRequest request,
+            [CosmosDB(
+                databaseName: "MaintenanceDB",
+                collectionName: "Vehicles",
+                ConnectionStringSetting = "CosmosDBConnection")] IEnumerable<Vehicle> vehicles,
+            ILogger log
+        )
+        {
+            log.LogInformation("Got all vehicles");
+            return vehicles;
         }
     }
 }
