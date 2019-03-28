3144f742133fa4f11948ecfcb382d0f8911d39b4
Chris Carpenter <chris@uberchris.com>
2019-03-28 14:01:30 -0500

    implement get maintenance by vehicle

--- maintenance-tracker-api/Functions/Maintenance.cs
+++ maintenance-tracker-api/Functions/Maintenance.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Linq;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -24,6 +28,26 @@ namespace maintenance_tracker_api.Functions
             maintenance.id = Guid.NewGuid();
             maintenance.UserId = principal.Identity.Name;
             log.LogInformation("Saving new maintenance");
+        }
+
+        [FunctionName("MaintenanceGetByVehicle")]
+        public static IActionResult MaintenanceGetByVehicle(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "maintenance")] HttpRequest request,
+            [CosmosDB(ConnectionStringSetting = "CosmosDBConnection")] DocumentClient client,
+            ILogger log,
+            ClaimsPrincipal principal
+        )
+        {
+            var collectionUri = UriFactory.CreateDocumentCollectionUri("MaintenanceDB", "Maintenance");
+            var maintenance = client.CreateDocumentQuery<Models.Maintenance>(collectionUri)
+                .Where(p => p.Vehicle == request.Query["vehicleId"].ToString()) //TODO: type vehicle to guid instead of string
+                .ToList();
+            if (maintenance.Any(x => x.UserId != principal.Identity.Name))
+            {
+                return new UnauthorizedResult();
+            }
+            log.LogInformation("Got all maintenance");
+            return new OkObjectResult(maintenance);
         }
     }
 }
--- maintenance-tracker-api/Models/Maintenance.cs
+++ maintenance-tracker-api/Models/Maintenance.cs
@@ -6,7 +6,7 @@ namespace maintenance_tracker_api.Models
     {
         // ReSharper disable once InconsistentNaming
         public Guid id { get; set; }
-        public Guid Vehicle { get; set; }
+        public string Vehicle { get; set; }
         public string UserId { get; set; }
         public string Item { get; set; }
         public string Mileage { get; set; }
